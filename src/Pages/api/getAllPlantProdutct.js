a226f3de2f4e40abf22be0f226144764521c87cb
Shubham Siddhapura <shubhs2844@gmail.com>
2025-06-11 11:44:28 +0530

    add

--- src/Pages/api/getAllPlantProdutct.js
+++
@@ -1,9 +0,0 @@
-
-const { getAllProductsBySection } = require("../../Controller/Section");
-
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    return getAllProductsBySection(req, res);
-  }
-  res.status(405).json({ error: "Only POST supported" });
-}
